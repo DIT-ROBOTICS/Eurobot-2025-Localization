11d1f021e4934cf4d1a696a4676d1926c6c5f5c6
wintera1233 <zhen.cc.1207@gmail.com>
2024-11-20 22:14:01 +0800

    Add: ros1 dockerfiles& ws

--- localization-ws/src/localization-devel-ws/device-starter.sh
+++ localization-ws/src/localization-devel-ws/device-starter.sh
@@ -7,3 +7,4 @@ cd $PACKAGES_PATH/local/imu/phidgets_drivers/phidgets_api
 sudo cp debian/udev /etc/udev/rules.d/99-phidgets.rules
 # source /install/setup.bash
 # ros2 launch phidgets_spatial spatial-launch.py 
+sudo chmod 777 /dev/ttyUSB0
