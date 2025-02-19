a4a6796bd8b30c13f43eb078e64602c6f2c894f8
wintera1233 <zhen.cc.1207@gmail.com>
2025-02-19 18:26:44 +0800

    Fix: wrong matrix index(ekf_node)

--- localization-devel-ws/ekf/ekf/ekf_node.py
+++ localization-devel-ws/ekf/ekf/ekf_node.py
@@ -53,16 +53,16 @@ class EKFFootprintBroadcaster(Node):
 
         self.X = np.array([0.0, 0.0, 0.0, 0.0, 0.0, 0.0])  # State vector: x, y, theta, vx, vy, w
         self.P = np.eye(6) * 9 * 1e-4
-        self.P[5, 5] = 0.003
-        self.P[2, 2] = 1e-6
+        self.P[2, 2] = 0.003
         self.P[3, 3] = 1e-6
         self.P[4, 4] = 1e-6
+        self.P[5, 5] = 1e-6
 
         self.Q = np.eye(6) * 5 * 1e-11
-        self.Q[5, 5] = 3 * 1e-5
-        self.Q[2, 2] = 1e-6
+        self.Q[2, 2] = 3 * 1e-5
         self.Q[3, 3] = 1e-6
         self.Q[4, 4] = 1e-6
+        self.Q[5, 5] = 1e-6
 
         self.R_gps = np.eye(3) * 1e-2
         self.R_camera = np.eye(3) * 1e-2
@@ -103,12 +103,12 @@ class EKFFootprintBroadcaster(Node):
             msg.pose.orientation.z,
             msg.pose.orientation.w
         )
-        self.X[5] = theta
+        self.X[2] = theta
         if msg.covariance[0] > 0:
             if msg.covariance[0] < 1:
                 self.P[0, 0] = msg.covariance[0]
                 self.P[1, 1] = msg.covariance[7]
-                self.P[5, 5] = msg.covariance[35]
+                self.P[2, 2] = msg.covariance[35]
         self.X[0] = msg.pose.position.x
         self.X[1] = msg.pose.position.y
 
@@ -178,7 +178,7 @@ class EKFFootprintBroadcaster(Node):
         w = msg.twist.twist.angular.z 
         # self.get_logger().info(f"dTime:{dt}, d_x:{delta_x}")
 
-        self.Q[5, 5] = msg.twist.covariance[35] 
+        self.Q[2, 2] = msg.twist.covariance[35] 
 
         self.ekf_predict(v_x, v_y, w, dt) 
 
@@ -193,8 +193,8 @@ class EKFFootprintBroadcaster(Node):
 
         self.X[0] += v_x * dt * math.cos(theta) - v_y * dt * math.sin(theta)
         self.X[1] += v_x * dt * math.sin(theta) + v_y * dt  * math.cos(theta)
-        self.X[5] += w * dt
-        self.X[5] = normalize_angle(self.X[5])
+        self.X[2] += w * dt
+        self.X[2] = normalize_angle(self.X[2])
        
         self.P = F @ self.P @ F.T + self.Q
      
@@ -208,12 +208,12 @@ class EKFFootprintBroadcaster(Node):
         H = np.zeros((3, 6))
         H[0, 0] = 1  
         H[1, 1] = 1 
-        H[2, 5] = 1
+        H[2, 2] = 1
         S = H @ self.P @ H.T + R
         K = self.P @ H.T @ np.linalg.inv(S)
 
         self.X += K @ (z - H @ self.X)
-        self.X[5] = normalize_angle(self.X[5])
+        self.X[2] = normalize_angle(self.X[2])
 
         I = np.eye(6)
         self.P = (I - K @ H) @ self.P @ (I - K @ H).T + K @ R @ K.T
@@ -232,7 +232,7 @@ class EKFFootprintBroadcaster(Node):
         t.transform.translation.x = self.X[0]
         t.transform.translation.y = self.X[1]
         t.transform.translation.z = 0.0
-        quat = quaternion_from_euler(0, 0, self.X[5])
+        quat = quaternion_from_euler(0, 0, self.X[2])
         t.transform.rotation.x = quat[0]
         t.transform.rotation.y = quat[1]
         t.transform.rotation.z = quat[2]
@@ -249,7 +249,7 @@ class EKFFootprintBroadcaster(Node):
         self.final_pose.pose.pose.orientation.w = quat[3]
         self.final_pose.pose.covariance[0] = self.P[0, 0]
         self.final_pose.pose.covariance[7] = self.P[1, 1]
-        self.final_pose.pose.covariance[35] = self.P[5, 5]
+        self.final_pose.pose.covariance[35] = self.P[2, 2]
         self.ekf_pose_publisher.publish(self.final_pose)
 
 
