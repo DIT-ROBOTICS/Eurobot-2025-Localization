fd9c7c42016bd6d268d362ec66e76059ff757d35
wintera1233 <zhen.cc.1207@gmail.com>
2025-03-27 22:34:09 +0800

    Add: single lidar triangular localization

--- localization-devel-ws/global/lidar_localization_pkg/lidar_localization_pkg/pred_publisher.py
+++ localization-devel-ws/global/lidar_localization_pkg/lidar_localization_pkg/pred_publisher.py
@@ -9,7 +9,7 @@ class PredPublisher(Node):
     def __init__(self):
         super().__init__('pred_publisher')
         self.pose_pred = PoseWithCovarianceStamped()
-        self.pose_pred.header.frame_id = 'map'
+        self.pose_pred.header.frame_id = "map"
         self.pose_pred.pose.pose.position.x = 0.0
         self.pose_pred.pose.pose.position.y = 0.0
         self.pose_pred.pose.pose.position.z = 0.0
@@ -22,13 +22,26 @@ class PredPublisher(Node):
         self.pose_pred.pose.covariance[7] = 0.0025
         self.pose_pred.pose.covariance[35] = 0.25
 
-        self.pub = self.create_publisher(PoseWithCovarianceStamped, '/pred_pose', 10)
-        self.sub = self.create_subscription(PoseWithCovarianceStamped, '/pred_pub_param', self.param_callback, 10)
+        self.pub = self.create_publisher(PoseWithCovarianceStamped, '/final_pose', 10)
+        self.sub = self.create_subscription(PoseWithCovarianceStamped, '/lidar_pose', self.param_callback, 10)
 
         self.br = TransformBroadcaster(self)
+        self.t = TransformStamped()
+        self.t.header.stamp = self.get_clock().now().to_msg()
+        self.t.header.frame_id = "map"
+        self.t.child_frame_id = "base_footprint"
+        self.t.transform.translation.x = 0.0
+        self.t.transform.translation.y = 1.0
+        self.t.transform.translation.z = 0.0
+        self.t.transform.rotation.x = 0.0
+        self.t.transform.rotation.y = 0.0
+        self.t.transform.rotation.z = 0.0
+        self.t.transform.rotation.w = 1.0
         self.timer = self.create_timer(0.01, self.publish_pose)  # 100Hz
+       
 
     def param_callback(self, msg):
+        # self.get_logger().info('Received parameter: %s' % msg)
         # Modify the pose_pred data based on the received parameter
         self.pose_pred.pose.pose.position.x = msg.pose.pose.position.x
         self.pose_pred.pose.pose.position.y = msg.pose.pose.position.y
@@ -38,23 +51,19 @@ class PredPublisher(Node):
         self.pose_pred.pose.pose.orientation.z = msg.pose.pose.orientation.z
         self.pose_pred.pose.pose.orientation.w = msg.pose.pose.orientation.w
         self.pose_pred.pose.covariance = msg.pose.covariance
-
+        self.publish_pose()
         # Broadcast static transform from map to robot base
-        t = TransformStamped()
-        t.header.stamp = self.get_clock().now().to_msg()
-        t.header.frame_id = 'map'
-        t.child_frame_id = 'robot_predict'
-        t.transform.translation.x = self.pose_pred.pose.pose.position.x
-        t.transform.translation.y = self.pose_pred.pose.pose.position.y
-        t.transform.translation.z = self.pose_pred.pose.pose.position.z
-        t.transform.rotation = self.pose_pred.pose.pose.orientation
-
-        self.br.sendTransform(t)
 
     def publish_pose(self):
         self.pose_pred.header.stamp = self.get_clock().now().to_msg()
         self.pub.publish(self.pose_pred)
-
+        self.t.header.stamp = self.get_clock().now().to_msg()
+        self.t.transform.translation.x = self.pose_pred.pose.pose.position.x
+        self.t.transform.translation.y = self.pose_pred.pose.pose.position.y
+        self.t.transform.translation.z = self.pose_pred.pose.pose.position.z
+        self.t.transform.rotation = self.pose_pred.pose.pose.orientation
+        self.br.sendTransform(self.t)
+        # self.get_logger().info('Publishing predicted pose')
 def main(args=None):
     rclpy.init(args=args)
     pred_publisher = PredPublisher()
