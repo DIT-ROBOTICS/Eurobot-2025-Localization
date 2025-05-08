bcd6c02a6c89653f6942fc1ae9c4ed67c2e27ea0
jossiewang <jossiew621@gapp.nthu.edu.tw>
2025-05-08 21:10:28 +0800

    Fix: (launch.py) some typo

--- localization-devel-ws/ekf/setup.py
+++ localization-devel-ws/ekf/setup.py
@@ -13,7 +13,7 @@ setup(
             ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
         ('share/' + package_name + '/launch', 
-         ['launch/' + f for f in os.listdir('launch') if f.endswith('.launch')])
+         [os.path.join('launch', f) for f in os.listdir('launch') if f.endswith('.launch')]),
         (os.path.join('share', package_name, 'launch'), glob('launch/*'))
     ],
     install_requires=['setuptools'],
