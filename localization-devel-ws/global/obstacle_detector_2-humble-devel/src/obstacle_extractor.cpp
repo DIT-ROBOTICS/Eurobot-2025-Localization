286b575e1302d4eaa731cf22ac755b98647d71eb
robert <robert940121@gmail.com>
2025-05-10 16:45:42 +0800

    Revert "Modify: pass input point to tail elimination"
    This reverts commit ff4ca42783c6a4b231e91958cae63faf1ed0c051.

--- localization-devel-ws/global/obstacle_detector_2-humble-devel/src/obstacle_extractor.cpp
+++ localization-devel-ws/global/obstacle_detector_2-humble-devel/src/obstacle_extractor.cpp
@@ -297,7 +297,8 @@ void ObstacleExtractor::groupPoints() {
       if (abs(sin_d) < sin_dp && range < prev_range)
         point_set.is_visible = false;
 
-      tailElimination(point_set, input_points_);
+      std::list<Point> point_set_list(point_set.begin, std::next(point_set.end));
+      tailElimination(point_set, point_set_list);
       detectSegments(point_set);
 
       // Begin new point set
@@ -308,7 +309,8 @@ void ObstacleExtractor::groupPoints() {
     }
   }
 
-  tailElimination(point_set, input_points_);
+  std::list<Point> point_set_list(point_set.begin, std::next(point_set.end));
+  tailElimination(point_set, point_set_list);
   detectSegments(point_set); // Check the last point set too!
 }
 
