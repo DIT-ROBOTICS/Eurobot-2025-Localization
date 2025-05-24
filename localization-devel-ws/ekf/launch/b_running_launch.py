629c7d2600e00a37b7695c3493b113c87b617201
wintera1233 <zhen.cc.1207@gmail.com>
2025-05-25 03:26:59 +0800

    Modified: (launch,image) param in launch, master

--- localization-devel-ws/ekf/launch/b_running_launch.py
+++ localization-devel-ws/ekf/launch/b_running_launch.py
@@ -10,12 +10,6 @@ def generate_launch_description():
     rival_name = LaunchConfiguration('rival_name')
     side = LaunchConfiguration('side')
 
-    rival_config_path = PathJoinSubstitution([
-        FindPackageShare('rival_localization'),
-        'config',
-        'rival_localization.yaml'
-    ])
-
     local_filter_launch_path = PathJoinSubstitution([
         FindPackageShare('local_filter'),
         'launch',
@@ -58,12 +52,12 @@ def generate_launch_description():
             'robot_parent_frame_id': 'map',
             'robot_frame_id': 'base_footprint',
             '/use_sim_time': False,
-            'q_linear': 1.2e-5,
-            'q_angular': 1.7e-6,
+            'q_linear': 1.2e-6,
+            'q_angular': 1.7e-7,
             'r_camra_linear': 1e-2,
-            'r_camra_angular': 0.15,
-            'r_threshold_xy': 1e-3,
-            'r_threshold_theta': 1e-2
+            'r_camra_angular': 0.1,
+            'r_threshold_xy': 1e-2,
+            'r_threshold_theta': 1e-1
         }],
         remappings=[
             ('initalpose', ['initial_pose'])
@@ -82,7 +76,8 @@ def generate_launch_description():
             'debug_mode': False,
             'visualize_candidate': True,
             'likelihood_threshold': 0.8,
-            'consistency_threshold': 0.95
+            'consistency_threshold': 0.95,
+            'lidar_multiplier': 0.987
         }]
     )
 
@@ -126,25 +121,38 @@ def generate_launch_description():
             }
         ],
         remappings=[
-            ('raw_pose', [rival_name, '/raw_pose']),
-            # ('final_pose', [rival_name, '/final_pose'])
+            ('raw_pose', [rival_name, '/raw_pose'])
         ]
     )
 
     rival_obstacle_node = Node(
-            package='obstacle_detector',
-            executable='obstacle_extractor_node',
-            name='obstacle_detector_to_map',
-            parameters=[
-                rival_config_path,
-                {'frame_id': 'map'},
-                {'pose_array':'true'}
-            ],
-            remappings=[
-                ('raw_obstacles', '/obstacles_to_map'),
-                ('scan', '/scan'),
-                ('raw_obstacles_visualization_pcl', 'raw_obstacle_visualization_to_map_pcl')
-            ]
+        package='obstacle_detector',
+        executable='obstacle_extractor_node',
+        name='obstacle_detector_to_map',
+        parameters=[{
+            'frame_id': 'map',
+            'active': True,
+            'use_scan': True,
+            'use_pcl': False,
+            'use_split_and_merge': True,
+            'circles_from_visibles': True,
+            'discard_converted_segments': False,
+            'transform_coordinates': True,
+            'min_group_points': 5,
+            'max_group_distance': 0.04,
+            'distance_proportion': 0.00628,
+            'max_split_distance': 0.02,
+            'max_merge_separation': 0.25,
+            'max_merge_spread': 0.02,
+            'max_circle_radius': 0.2,
+            'radius_enlargement': 0.05,
+            'pose_array': True
+        }],
+        remappings=[
+            ('raw_obstacles', '/obstacles_to_map'),
+            ('scan', '/scan'),
+            ('raw_obstacles_visualization_pcl', 'raw_obstacle_visualization_to_map_pcl')
+        ]
     )
 
     static_tf = Node(
@@ -154,7 +162,7 @@ def generate_launch_description():
         output='screen',
         arguments=[
             '--x', '0', '--y', '0', '--z', '0',
-            '--roll', '0', '--pitch', '0', '--yaw', '-1.57534',
+            '--roll', '0', '--pitch', '0', '--yaw', '-1.633628',
             '--frame-id', 'base_footprint',
             '--child-frame-id', 'laser'
         ]
