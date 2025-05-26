06e2c48499e066f93888a7838e812110525e7299
robert <robert940121@gmail.com>
2025-05-26 14:08:07 +0800

    Modify: reset to ok version

--- localization-devel-ws/healthcheck/healthcheck/healthcheck.py
+++ localization-devel-ws/healthcheck/healthcheck/healthcheck.py
@@ -185,7 +185,6 @@ class HealthCheckNode(Node):
         self.lidar_param_pub.publish(self.point_msg)
         self.get_logger().info("Lidar parameters set to running")
         # 6. response to main (a service?)
-        self.get_logger().info("send ready signal")
         self.ready_signal.sendReadySignal(4, 3)
         return True
     
